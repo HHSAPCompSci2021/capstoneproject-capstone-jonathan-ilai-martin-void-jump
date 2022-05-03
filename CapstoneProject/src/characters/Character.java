409bdc445ecb06ce20f9b1bf8da5211e197a8f63
Jonathan Levi <jlevi305@student.fuhsd.org>
2022-05-03 10:35:05 -0700

    Update Character.java

--- CapstoneProject/src/characters/Character.java
+++ CapstoneProject/src/characters/Character.java
@@ -1,14 +1,25 @@
 package characters;
 
+import processing.core.PApplet;
+import processing.core.PImage;
+
 public class Character {
 
+	private double x, y, width, height;
+	private PImage image;
 	
-	
-	public Character() {
-		
+	public Character(PImage image, double width, double height, int x, int y) {
+		this.image = image;
+		this.width = width;
+		this.height = height;
+
+		this.x = x;
+		this.y = y;
 	}
 	
-	
+	public void draw(PApplet surface) {
+		if (image != null) surface.image(image, (float) x, (float) y);
+	}
 	
 	
 	
