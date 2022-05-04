78f5b4dbce70ae4b9b583203cc262d6c9a2a2cff
Ilai Tamari <itamari184@student.fuhsd.org>
2022-05-03 17:00:20 -0700

    Added wizard icon

--- CapstoneProject/src/characters/Person.java
+++ CapstoneProject/src/characters/Person.java
@@ -1,27 +1,34 @@
 package characters;
 
 
+import core.DrawingSurface;
 import processing.core.PApplet;
 import processing.core.PImage;
 
-public class Character {
+public class Person {
 
 	private double x, y, width, height;
 	private PImage image;
 	
-	public Character(PImage image, double width, double height, int x, int y) {
-		this.image = image;
+	public Person(double width, double height, int x, int y) {
 		this.width = width;
 		this.height = height;
-
 		this.x = x;
 		this.y = y;
 	}
+	
+	public void setIcon(DrawingSurface surface, String address) {
+		image = surface.loadImage(address);
+		image.resize(50, 80);
+	}
+	
 	public void draw(PApplet surface) {
 		if (image != null) surface.image(image, (float) x, (float) y);
 	}
 	
-	
+	public PImage getIcon() {
+		return image;
+	}
 	
 	
 	
--- CapstoneProject/src/screens/Level.java
+++ CapstoneProject/src/screens/Level.java
@@ -3,6 +3,7 @@ package screens;
 import java.awt.Point;
 import java.awt.geom.Ellipse2D;
 
+import characters.Person;
 import core.DrawingSurface;
 import platforms.Platform;
 import processing.core.PImage;
@@ -13,6 +14,7 @@ public abstract class Level extends Screen {
 	 protected Platform[] platforms;
 	 protected PImage returnIcon;
 	 protected Ellipse2D returnButton;
+	 private Person player;
 	 
 	 
 	 public Level(DrawingSurface surface, double startX, double startY, double keyX, double keyY, double gateX1, double gateX2) {
@@ -23,14 +25,17 @@ public abstract class Level extends Screen {
 		 this.keyY = keyY;
 		 this.gateX = gateX;
 		 this.gateY = gateY;
+		 player = new Person(0, 0, 0, 0);
 		 platforms = new Platform[10];
 		 returnButton = new Ellipse2D.Double(10, 10, 50, 50);
 	 }
 	 
 	 public void setup() {
-		 returnIcon = surface.loadImage("img/return.png");
-			returnIcon.resize(50, 50);
+		returnIcon = surface.loadImage("img/return.png");
+		returnIcon.resize(50, 50);
+		player.setIcon(surface, "img/Wizard.png");
 		addPlatforms();
+		
 	 }
 
 	 protected abstract void addPlatforms();
@@ -38,6 +43,7 @@ public abstract class Level extends Screen {
 	 public void draw() {
 		 surface.background(0);
 		 surface.image(returnIcon, 10, 10);
+		 surface.image(player.getIcon(), 50, 50);
 		 for (Platform platform : platforms) {
 			 if (platform != null) platform.draw(surface);
 		 }
