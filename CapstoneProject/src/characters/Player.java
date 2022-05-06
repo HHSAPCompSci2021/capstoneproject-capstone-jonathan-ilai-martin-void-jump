832195901ac6f3459dc8e71247d40476b7907bec
Ilai Tamari <itamari184@student.fuhsd.org>
2022-05-06 09:49:12 -0700

    Added key

--- CapstoneProject/src/characters/Player.java
+++ CapstoneProject/src/characters/Player.java
@@ -1,16 +1,19 @@
 package characters;
 
-import core.DrawingSurface;
 import processing.core.PImage;
 
 public class Player extends Person {
 	
 	public static double WIDTH = 70, HEIGHT = 100;
+	private boolean hasKey;
 
 	public Player(PImage image, double x, double y) {
 		super(image, x, y, WIDTH, HEIGHT);
 	}
 	
+	public boolean hasKey() {
+		return hasKey;
+	}
 	
 	public double getX() {
 		return x;
@@ -18,6 +21,10 @@ public class Player extends Person {
 	
 	public double getY() {
 		return y;
+	}
+	
+	public void takeKey() {
+		hasKey = true;
 	}
 
 }
