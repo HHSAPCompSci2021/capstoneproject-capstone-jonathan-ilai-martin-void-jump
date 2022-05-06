a99c20a98595df544a22834962579ef199ce8088
Jonathan Levi <jlevi305@student.fuhsd.org>
2022-05-06 09:39:13 -0700

    Update Portal.java

--- CapstoneProject/src/portals/Portal.java
+++ CapstoneProject/src/portals/Portal.java
@@ -6,6 +6,7 @@ import characters.Player;
 import core.DrawingSurface;
 import processing.core.PApplet;
 import processing.core.PImage;
+import java.awt.Point;
 
 
 
@@ -16,15 +17,20 @@ public class Portal extends Rectangle2D.Double {
 	public final double WIDTH = 70, HEIGHT = 100;
 	public double x, y;
 	private PImage image;
+	private Rectangle2D.Double body;
 
 	
 	public Portal(PImage image, double x, double y) {
 		this.image = image;
 		this.x = x;
 		this.y = y;
+		body = new Rectangle2D.Double(x, y, WIDTH, HEIGHT);
+
 	}
 	
-	
+	public Rectangle2D.Double getPortal() {
+		return body;
+	}
 	
 	public void draw(PApplet surface) {
 		if (image != null)
@@ -34,9 +40,12 @@ public class Portal extends Rectangle2D.Double {
 	
 	public boolean isInside(Player p) {
 		
-		Point p = new Point(p)
+		Point center = new Point((int)(p.getX() + 35), (int)(p.getY() + 50));
 		
-		
+		Rectangle2D.Double portalBody = this.getPortal();
+		if (portalBody.contains(center)) {
+			return true;
+		}
 		return false;
 	}
 	
