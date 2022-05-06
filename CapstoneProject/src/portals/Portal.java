7d4fad7954a666d0bad17853f140060c6b3f360b
Jonathan Levi <jlevi305@student.fuhsd.org>
2022-05-06 09:26:45 -0700

    update

--- CapstoneProject/src/portals/Portal.java
+++ CapstoneProject/src/portals/Portal.java
@@ -1,23 +1,36 @@
 package portals;
 
+import java.awt.geom.Rectangle2D;
+
 import core.DrawingSurface;
+import processing.core.PApplet;
 import processing.core.PImage;
 
 
 
-public class Portal {
+public class Portal extends Rectangle2D.Double {
 
 	
 	
 	public final double WIDTH = 70, HEIGHT = 100;
+	public double x, y;
+	private PImage image;
+
 	
-	public Portal() {
-		
+	public Portal(PImage image, double x, double y) {
+		this.image = image;
+		this.x = x;
+		this.y = y;
 	}
 	
 	
 	
-	public 
+	public void draw(PApplet surface) {
+		if (image != null)
+			surface.image(image, (float) x, (float) y, (float)WIDTH,(float)HEIGHT);
+	}
+	
+	
 	
 	
 	
