032b257b471119bd503d5f11202d736decc976b6
Ilai Tamari <itamari184@student.fuhsd.org>
2022-05-10 10:17:13 -0700

    Update Lazer.java

--- CapstoneProject/src/characters/Lazer.java
+++ CapstoneProject/src/characters/Lazer.java
@@ -4,17 +4,22 @@ import java.awt.Color;
 
 import ilaitm12.shapes.Line;
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Lazer extends Line {
+	
+	private PImage icon;
+	private double angle;
 
-	public Lazer(double x, double y, double angle, double length) {
+	public Lazer(PImage image, double x, double y, double angle, double length) {
 		super(x, y, x + length * Math.cos(angle * Math.PI / 180), y - length * Math.sin(angle * Math.PI / 180));
+		icon = image;
 		this.setColor(Color.WHITE, Color.WHITE);
 		this.setStrokeWeight(6);
 	}
 	
-//	public void draw(PApplet surface) {
-//		surface.strokeWeight(6);
-//		super.draw(surface);
-//	}
+	public void draw(PApplet surface) {
+		surface.rotate((float) angle);
+		surface.image(icon, 0, 0);
+	}
 }
