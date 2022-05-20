28c5f3c6421454881da6a6e3db2bd82e64435db4
Martin Simeonov <89154040+msimeonov254@users.noreply.github.com>
2022-05-20 09:55:54 -0700

    Spike Javadoc

--- CapstoneProject/src/platforms/Spikes.java
+++ CapstoneProject/src/platforms/Spikes.java
@@ -1,9 +1,22 @@
 package platforms;
 
 import processing.core.PImage;
-
+/**
+ * 
+ * A platform type that represents a set of spikes that characters can't step on, otherwise they 'die'.
+ * @author Martin Simeonov
+ *
+ */
 public class Spikes extends Platform{
 
+	/**
+	 * Constructs a new set of spikes
+	 * @param image The image that will be used to show the spikes
+	 * @param x The starting x-coordinate of the spike set
+	 * @param y The starting y-coordinate of the spike set
+	 * @param width The width of the spike set
+	 * @param height The height of the spike set
+	 */
 	public Spikes(PImage image, double x, double y, double width, double height) {
 		super(image, x, y, width, height);
 	}
